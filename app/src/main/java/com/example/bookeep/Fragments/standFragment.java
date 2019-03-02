2445cf66ca6fb77767305ba03addf0f57c604d29
Jeffrey Kirker <jkirker@ualberta.ca>
2019-03-02 16:21:47 -0700

    Revert "Navigation"

--- app/src/main/java/com/example/bookeep/Fragments/standFragment.java
+++ app/src/main/java/com/example/bookeep/Fragments/standFragment.java
@@ -10,10 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.bookeep.Book;
 import com.example.bookeep.R;
+import com.example.bookeep.Fragments.dummy.DummyContent;
+import com.example.bookeep.Fragments.dummy.DummyContent.DummyItem;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A fragment representing a list of Items.
@@ -21,25 +22,25 @@ import java.util.ArrayList;
  * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
-public class ShelfFragment extends Fragment {
-    ArrayList<Book> BookList;
-    MyShelfRecyclerViewAdapter adapter;
+public class standFragment extends Fragment {
+
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-
+    // TODO: Customize parameters
+    private int mColumnCount = 2;
     private OnListFragmentInteractionListener mListener;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public ShelfFragment() {
+    public standFragment() {
     }
 
     // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
-    public static ShelfFragment newInstance(int columnCount) {
-        ShelfFragment fragment = new ShelfFragment();
+    public static standFragment newInstance(int columnCount) {
+        standFragment fragment = new standFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
@@ -49,25 +50,28 @@ public class ShelfFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        BookList = MyShelfRecyclerViewAdapter.createBookList(6);
-
-        View view = inflater.inflate(R.layout.fragment_shelf_list, container, false);
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.shelf_recycler_view);
+        View view = inflater.inflate(R.layout.fragment_stand_list, container, false);
 
         // Set the adapter
-        adapter = new MyShelfRecyclerViewAdapter(BookList, mListener);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(adapter);
-
-        //Set grid layout for cards
-        Context context = view.getContext();
-        recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
-
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new MystandRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+        }
         return view;
     }
 
@@ -101,6 +105,6 @@ public class ShelfFragment extends Fragment {
      */
     public interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onListFragmentInteraction(Book item);
+        void onListFragmentInteraction(DummyItem item);
     }
 }
