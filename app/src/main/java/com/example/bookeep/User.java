735f3f56203187675117663ea01c384032681c47
nafeekhan <nafee@ualberta.ca>
2019-03-28 19:19:31 -0600

    return book by scanning; two way scan transaction implemented

--- app/src/main/java/com/example/bookeep/User.java
+++ app/src/main/java/com/example/bookeep/User.java
@@ -179,6 +179,16 @@ public class User implements Serializable {
         }
 
     }
+    public void removeFromBorrowed(String bookId){
+
+        for(int i = 0; i < borrowedIds.size(); i++){
+            if(borrowedIds.get(i).equals(bookId)){
+
+                borrowedIds.remove(i);
+            }
+        }
+
+    }
 
     /**
      * gets owned book ids
