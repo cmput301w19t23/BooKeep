d98dc3d24e55f37ffe07c147af5381e7052cc880
Hugh Bagan <hbagan@ualberta.ca>
2019-03-30 15:29:31 -0600

    doc'd DownloadImageTask.java

--- app/src/main/java/com/example/bookeep/DownloadImageTask.java
+++ app/src/main/java/com/example/bookeep/DownloadImageTask.java
@@ -7,7 +7,16 @@ import android.util.Log;
 
 import java.io.InputStream;
 
+/**
+ * Used to download images for ImageViews
+ * @author Jeff Kirker
+ */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    /**
+     * Downloads a bitmap in the background and returns it
+     * @param urls the image's URL to download
+     * @return The specified image
+     */
     protected Bitmap doInBackground(String... urls) {
 
         String urldisplay = urls[0];
@@ -22,5 +31,10 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         }
         return mIcon11;
     }
+
+    /**
+     *
+     * @param result
+     */
     protected void onPostExecute(Bitmap result) {}
 }
