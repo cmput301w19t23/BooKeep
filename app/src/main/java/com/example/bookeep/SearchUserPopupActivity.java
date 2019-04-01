6ab1adb2218b6d0fed467e52e5fa00abf01f6820
Nolan Brost <nbrost@ualberta.ca>
2019-03-31 20:20:42 -0600

    checkingout development

--- app/src/main/java/com/example/bookeep/SearchUserPopupActivity.java
+++ app/src/main/java/com/example/bookeep/SearchUserPopupActivity.java
@@ -3,6 +3,7 @@ package com.example.bookeep;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
